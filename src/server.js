3caaff0e9725c9cd91c797220924007589092007
amityadav545 <amitky545@gmail.com>
2024-05-16 02:42:17 +0530

    basic changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const dataRoutes = require('./src/routes/dataRoutes');
+const dataRoutes = require('./routes/dataRoutes');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
